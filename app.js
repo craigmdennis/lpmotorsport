f1e1cc8a744abd921e81c7959eb94f6f5fac37fb
Craig Dennis <craigmdennis@gmail.com>
2018-04-04 23:02:35 +0100

    Add Contentful

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const sugarml = require('sugarml')
 const sugarss = require('sugarss')
+const Contentful = require('spike-contentful')
+const locals = {}
 const env = process.env.SPIKE_ENV
 
 module.exports = {
+  plugins: [
+    new Contentful({
+      addDataTo: locals,
+      accessToken: '6d405922a8ad8bea14ce60958e8c35c94ee3d51d5a08d0c2de6c62d09acd15cb',
+      spaceId: 'p7deyor9gd4e',
+      contentTypes: [
+        {
+          name: 'vehicles',
+          id: 'vehicle'
+        }
+      ]
+    })
+  ],
   devtool: 'source-map',
   matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
   reshape: htmlStandards({
     parser: sugarml,
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+    locals: () => locals,
     minify: env === 'production'
   }),
   postcss: cssStandards({
